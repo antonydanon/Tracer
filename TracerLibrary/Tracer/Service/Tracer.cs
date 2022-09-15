45afdeb0d38a27bf80782986ca073b270eb7fba9
Anton <antondanik32@mail.ru>
2022-09-15 18:44:47 +0300

    Create Itracer implementation

---
+++ TracerLibrary/Tracer/Service/Tracer.cs
@@ -0,0 +1,22 @@
+﻿using TracerLibrary.Tracer.Model;
+
+namespace TracerLibrary.Tracer.Service
+{
+    public class Tracer : ITracer
+    {
+        public void StartTrace()
+        {
+            
+        }
+
+        public void StopTrace()
+        {
+            
+        }
+
+        public TraceResult GetTraceResult()
+        {
+            return new TraceResult();
+        }
+    }
+}
