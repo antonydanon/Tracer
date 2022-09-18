6a7833dd3783126efcb6d52cd1560db81845b38d
Anton <antondanik32@mail.ru>
2022-09-18 12:45:42 +0300

    Add implementation JsonSerializer for interface ISerializer

---
+++ TracerLibrary/Serialization/JsonSerializer/JsonSerializer.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+using System.Text;
+using TracerLibrary.Tracer.Model;
+
+namespace TracerLibrary.Serialization.JsonSerializer
+{
+    public class JsonSerializer : ISerializer
+    {
+        public void Serialize(TraceResult traceResult, Stream to)
+        {
+            var res = System.Text.Json.JsonSerializer.Serialize(traceResult);
+            to.Write(Encoding.Default.GetBytes(res));
+        }
+    }
+}
